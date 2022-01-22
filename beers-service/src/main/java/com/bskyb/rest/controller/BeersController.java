c818d18f04cbb170571e111a671e0b8ffcd37b14
Sergi <sergi_parisi@hotmail.com>
2022-01-22 11:54:42 +0100

    sergi - Adding dependencies for spring security

--- beers-service/src/main/java/com/bskyb/rest/controller/BeersController.java
+++ beers-service/src/main/java/com/bskyb/rest/controller/BeersController.java
@@ -1,4 +1,4 @@
-package com.bskyb.rest;
+package com.bskyb.rest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
